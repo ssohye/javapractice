3762e7d75b4ccdb089b5789aed49a2d95bbbf065
ssohye <ssohye@icloud.com>
2022-03-04 16:30:04 +0900

    Changes

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    //entry point of program
+    public static void main(String[] args){
+        int x=1;
+        int y=2;
+        int a=x+y;
+        System.out.println(x+"+"+y+"="+a);
+    }//end of main()
+}
+
+
+
